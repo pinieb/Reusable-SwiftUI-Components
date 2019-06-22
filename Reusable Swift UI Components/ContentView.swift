a77708807915d9ee6680878169bc96065ecd6dcd
Pete Biencourt <piebie@microsoft.com>
2019-06-22 11:28:34 -0400

    Initial Commit

---
+++ Reusable Swift UI Components/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Reusable Swift UI Components
+//
+//  Created by Pete Biencourt on 6/22/19.
+//  Copyright © 2019 piebie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
